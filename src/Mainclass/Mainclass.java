8e12caf21bf4e8d16614c18984c3f5a27134ab9f
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-11-16 14:06:23 +0530

    initial commit scanner class used number enter

---
+++ src/Mainclass/Mainclass.java
@@ -0,0 +1,11 @@
+package Mainclass;
+
+import java.util.Scanner;
+
+public class Mainclass {
+    public static void main(String args[]) {
+        System.out.println("enter a number");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
